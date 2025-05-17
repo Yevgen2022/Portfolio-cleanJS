0b4cd68237e880c581dcff94d65a19c77f7280a2
Yevhen Oshkukov <sy.yevgen.lc@gmail.com>
2025-05-17 22:33:39 +0200

    Delete all unneccessary files

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,26 @@
-export default function Footer()
-{
+import {FaGithub, FaLinkedin, FaEnvelope} from "react-icons/fa";
+
+
+export default function Footer() {
     return (
         <footer
             className="dark-bg-tx text-center p-6 border-t text-gray-500   dark:border-gray-700">
             <div className="max-w-6xl mx-auto">
+
                 <div className="flex justify-center space-x-6 mb-4">
                     <a href="https://github.com/Yevgen2022?tab=repositories" target="_blank"
                        className="text-gray-400 hover:text-blue-600">
-                        <i className="fab fa-github text-xl"></i>
+                        <FaGithub className="text-xl"/>
                     </a>
                     <a href="https://www.linkedin.com/in/yevhen-oshkukov-436973254/" target="_blank"
                        className="text-gray-400 hover:text-blue-600">
-                        <i className="fab fa-linkedin text-xl"></i>
+                        <FaLinkedin className="text-xl"/>
                     </a>
                     <a href="mailto:sy.yevhen.lc@gmail.com" className="text-gray-400 hover:text-blue-600">
-                        <i className="far fa-envelope text-xl"></i>
+                        <FaEnvelope className="text-xl"/>
                     </a>
                 </div>
+
                 <p>&copy; 2025 Yevhen Oshkukov — Frontend Developer</p>
             </div>
         </footer>
